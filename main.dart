36617c790eff3e33b085e64ce6c69cd60d3aa24f
Felipe Meira <124220475+OFelipeMeira@users.noreply.github.com>
2023-07-04 23:02:46 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,26 +4,95 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('my title'),
+        title: Text('Top app bar'),
         backgroundColor: Colors.blueGrey[900],
       ),
       backgroundColor: Colors.blueGrey,
-      body: Center(
-        child: Image(
-          image: NetworkImage(
-              'https://media.istockphoto.com/id/851493796/pt/foto/close-up-of-kebab-sandwich.jpg?s=612x612&w=0&k=20&c=kHij2x8Wbd8Ao9Wip1D4d4-9bmel92WuwDv0kVj5rGk='),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+          Text("Name of the task",
+            style: TextStyle(
+              color: Colors.black,
+              fontStyle: FontStyle.normal,
+              height: 5, // height of the line
+              fontSize: 20,
+            ),
+          ),
+
+
+          
+          Container(
+            height: 50,
+            width: 150,
+            color: Colors.green,
+            child:
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Name of the item',
+              ),
+            ),
+          ),
+
+          Container(
+            height: 50,
+            width: 50,
+            color: Colors.greenAccent,
+          ),
+
+          Container(
+            height: 50,
+            width: 50,
+            color: Colors.lightGreen,
+          ),
+        ],
+
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+
+        currentIndex: 0,
+
+        type: BottomNavigationBarType.shifting,
+        iconSize: 30,
+        selectedFontSize: 15,
+        unselectedFontSize: 15,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.blue
+          ),
+
+          BottomNavigationBarItem(
+              icon: Icon(Icons.camera_alt),
+              label: 'Scan',
+              backgroundColor: Colors.blue
+          ),
+
+          BottomNavigationBarItem(
+              icon: Icon(Icons.description),
+              label: 'Item description',
+              backgroundColor: Colors.blue
+          ),
+
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'New item',
+              backgroundColor: Colors.blue
+          ),
+
+        ],
+      ),
+
     ),
   ));
 }
 
-/* #1
-  runApp(MaterialApp(
-      home: Center(
-        child: Text(
-          'HelloWord',
-        ),
-      ),
-    ));
-*/
+// https://www.youtube.com/watch?v=elLkVWt7gRM
+// Navbottom Bar
+
+//https://www.youtube.com/watch?v=1Iiq4oPCz3k
+//COlumns & Rows
